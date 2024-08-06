3a41ea3fee7bcc482c8942c96885afa526824b5b
sk0990670 <sk0990670@gmail.com>
2024-08-06 16:55:26 +0530

    first commit

---
+++ src/main/java/com/solo/sahej/App.java
@@ -0,0 +1,13 @@
+package com.solo.sahej;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
